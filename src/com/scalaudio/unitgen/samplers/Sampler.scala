b446bcd54a821409aa50b81eae3edbaf540b2099
John McGill <jmcg.music@gmail.com>
2016-02-03 23:22:12 -0500

    could finish Sampler multi-sample impl but it really seems like more complexity than creating a list of single clip Samplers, going to refactor towards single

--- src/com/scalaudio/unitgen/samplers/Sampler.scala
+++ src/com/scalaudio/unitgen/samplers/Sampler.scala
@@ -3,23 +3,24 @@ package com.scalaudio.unitgen.samplers
 import com.scalaudio.AudioContext
 import com.scalaudio.engine.Playback
 import com.scalaudio.math._
-import com.scalaudio.syntax.{ScalaudioSyntaxHelpers, UnitParams}
+import com.scalaudio.syntax.{AudioDuration, ScalaudioSyntaxHelpers, UnitParams}
 import com.scalaudio.unitgen.UnitGen
 
 import scala.concurrent.duration._
 /**
   * Created by johnmcgill on 1/5/16.
   */
-case class Sampler(val rawSamples : List[WavetableType])(implicit audioContext: AudioContext) extends UnitGen with ScalaudioSyntaxHelpers {
+case class Sampler(val sampleDeck : List[SampleDeck])(implicit audioContext: AudioContext) extends UnitGen with ScalaudioSyntaxHelpers {
   import SamplerUtils._
 
-  val soundSamples : Map[String, SamplerTape] = (rawSamples.zipWithIndex map {case (x,i) => (i.toString, SamplerTape(wavetableMode2Sample(x, 10 seconds), new SampleState()))}).toMap
+  val soundSamples : Map[String, SamplerTape] = (sampleDeck.zipWithIndex map {case (deck,i) => (deck.label, SamplerTape(wavetableMode2Sample(deck.wtType, 10 seconds), new SampleState(), deck.triggerTimes))}).toMap
 
   val sampleTape = soundSamples.head._2
   internalBuffers = List.fill(sampleTape.soundSample.wavetable.size)(Array.fill(audioContext.config.FramesPerBuffer)(0))
 
   override def computeBuffer(params : Option[UnitParams] = None) =
     0 until audioContext.config.FramesPerBuffer foreach { s =>
+      triggerCheck((currentFrame buffers) + AudioDuration(s))
       sampleTape.soundSample.wavetable.indices foreach { c =>
         internalBuffers(c)(s) = computeSample(c, sampleTape.state.position, 1)
       }
@@ -27,13 +28,15 @@ case class Sampler(val rawSamples : List[WavetableType])(implicit audioContext: 
     }
 
   def computeSample(channel : Int, sIndex : Double, clipIndex : Int) : Double = {
-    if (sIndex > sampleTape.soundSample.wavetable.head.length - 1) sampleTape.state.reset
+    if (sIndex > sampleTape.soundSample.wavetable.head.length - 1) sampleTape.state.reset()
     if (sampleTape.state.active) interpolatedSample(channel, sIndex) else 0
   }
 
+  def triggerCheck(currentTime : AudioDuration) =
+    soundSamples.values.filter(_.triggerTimes.exists(_ == currentTime)).foreach(_.state.activate())
+
   def activateSoundSample(sampleId : String) =
-    sampleTape.state.activate
-//    soundSamples.get(sampleId) foreach (_._2.active = true)
+    soundSamples.get(sampleId) foreach (_.state.activate())
 
   //TODO : De-dupe this (it's in WavetableGen as well)
   def interpolatedSample(channel : Int, position : Double) : Double = {
@@ -43,19 +46,21 @@ case class Sampler(val rawSamples : List[WavetableType])(implicit audioContext: 
   }
 }
 
-case class SamplerTape(soundSample: SoundSample, var state : SampleState)(implicit audioContext: AudioContext) {
+case class SampleDeck(label : String, wtType : WavetableType, triggerTimes : Option[List[AudioDuration]] = None)
+
+case class SamplerTape(soundSample: SoundSample, var state : SampleState, triggerTimes : Option[List[AudioDuration]])(implicit audioContext: AudioContext) {
   val incrementRate : Double = state.playbackRate * (soundSample.samplingFreq / audioContext.config.SamplingRate)
 }
 
 //TODO: incrementRate needs to be dynamically determined a la playbackRate * (sample.samplingFreq / audioContext.config.SamplingRate)
 case class SampleState(var playbackRate : Double = 1, var active : Boolean = false, var position : Double = 0) {
-  def reset = {
+  def reset() = {
     active = false
     position = 0
   }
 
-  def activate = {
-    reset
+  def activate() = {
+    reset()
     active = true
   }
 }
