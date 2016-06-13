d020ad11d903ba53bfabfd1ebdd8aefa63389e45
John McGill <jmcg.music@gmail.com>
2016-06-12 20:46:11 -0400

    setup CORS filter to fix front-end restriction

--- scalaudio-rpc/build.sbt
+++ scalaudio-rpc/build.sbt
@@ -1,4 +1,5 @@
 libraryDependencies in ThisBuild ++= Seq(
   "org.apache.thrift" % "libthrift" % "0.9.3",
-  "org.eclipse.jetty" % "jetty-servlet" % "9.4.0.M0"
+  "org.eclipse.jetty" % "jetty-servlet" % "9.4.0.M0",
+  "org.eclipse.jetty" % "jetty-servlets" % "9.4.0.M0"
 )
--- scalaudio-rpc/src/main/scala/com/scalaudio/rpc/JsonServer.scala
+++ scalaudio-rpc/src/main/scala/com/scalaudio/rpc/JsonServer.scala
@@ -1,10 +1,14 @@
 package com.scalaudio.rpc
 
+import java.util
+import javax.servlet.DispatcherType
+
 import com.scalaudio.rpc.thrift.generated.ScalaudioService
 import org.apache.thrift.protocol.TJSONProtocol
 import org.apache.thrift.server.TServlet
 import org.eclipse.jetty.server.Server
-import org.eclipse.jetty.servlet.{ServletHandler, ServletHolder}
+import org.eclipse.jetty.servlet.{FilterHolder, ServletHandler, ServletHolder}
+import org.eclipse.jetty.servlets.CrossOriginFilter
 
 object JsonServer extends App {
 
@@ -12,6 +16,14 @@ object JsonServer extends App {
   val server: Server = new Server(9091)
   val handler: ServletHandler = new ServletHandler
   server.setHandler(handler)
+
+  // Add the filter, and then use the provided FilterHolder to configure it
+  val cors = new FilterHolder(classOf[CrossOriginFilter]) // ,"/*",util.EnumSet.of(DispatcherType.REQUEST)
+  cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*")
+  cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*")
+  cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,POST,HEAD")
+  cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin")
+  handler.addFilterWithMapping(cors, "/*", 0)
 
   val holder = new ServletHolder(new TServlet(processor, new TJSONProtocol.Factory()))
   handler.addServletWithMapping(holder, "/*")
