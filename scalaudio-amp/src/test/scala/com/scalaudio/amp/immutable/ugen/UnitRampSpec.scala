68e0ee43009a64b81e04ab807e68f5b5d28ba8a8
John McGill <jmcg.music@gmail.com>
2016-07-11 02:34:47 -0400

    phasor, unit ramp (just 0 to 1 phasor instead of 2 pi basically), rescaler

---
+++ scalaudio-amp/src/test/scala/com/scalaudio/amp/immutable/ugen/UnitRampSpec.scala
@@ -0,0 +1,22 @@
+package com.scalaudio.amp.immutable.ugen
+
+import com.scalaudio.core.{AudioContext, ScalaudioCoreTestHarness}
+
+/**
+  * Created by johnmcgill on 7/10/16.
+  */
+class UnitRampSpec extends ScalaudioCoreTestHarness {
+  "1 Hz unit ramp" should "output 0 to 1 over 1 second + 1 sample" in {
+    implicit val audioContext = AudioContext()
+
+    val oscState = new OscState(0, 1.Hz, 0)
+
+    val secondPlusOneOfStates = (1 to audioContext.config.samplingRate + 1).scanLeft(oscState){(curr, acc) =>
+      UnitRamp.nextState(curr)
+    }.tail
+
+    secondPlusOneOfStates.length shouldEqual (audioContext.config.samplingRate + 1)
+    secondPlusOneOfStates.head.sample shouldEqual 0
+    Math.abs(secondPlusOneOfStates.last.sample - 1) should be < .000001
+  }
+}
