031ff8ae9a4a5ccbf30bbdf3c181467e6e037d76
John McGill <jmcg.music@gmail.com>
2016-12-10 01:35:16 -0500

    failed type experiments, should probably just use macro if really want to reduce LOC for this pattern

--- scalaudio-amp/src/main/scala/scalaudio/units/ugen/ImmutableSine.scala
+++ scalaudio-amp/src/main/scala/scalaudio/units/ugen/ImmutableSine.scala
@@ -6,7 +6,7 @@ import scalaudio.core.AudioContext
   * Created by johnmcgill on 5/27/16.
   */
 
-object ImmutableSine extends Osc {
+trait ImmutableSine extends Osc {
   def nextState(current: OscState)(implicit audioContext: AudioContext) : OscState = {
     val w = 2 * Math.PI * current.pitch.toHz / audioContext.config.samplingRate
 
@@ -16,3 +16,6 @@ object ImmutableSine extends Osc {
     )
   }
 }
+
+// Note: If we want to retain "object purity", can always create an object a la:
+//object ImmutableSineDefinitions extends ImmutableSine
